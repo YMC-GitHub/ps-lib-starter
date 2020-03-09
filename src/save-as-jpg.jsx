8492b029734d5550ab4bb0d4c9b5c3d4980799a9
yemiancheng <ymc.github@gmail.com>
2020-03-09 22:27:32 +0800

    build: update save as jpg jsx

--- src/save-as-jpg.jsx
+++ src/save-as-jpg.jsx
@@ -3,19 +3,53 @@
 
 #include config.jsx
 
+
+function SaveJPG(saveFile, quality){
+	jpgSaveOptions = new JPEGSaveOptions();
+
+	// embedColorProfile (True to embed the color profile in the document.)
+	jpgSaveOptions.embedColorProfile = true;
+
+	// formatOptions (The download format to use.)
+	// Default: FormatOptions.STANDARDBASELINE
+	// Range: FormatOptions.STANDARDBASELINE,
+	//			FormatOptions.OPTIMIZEDBASELINE,
+	//			FormatOptions.PROGRESSIVE
+	jpgSaveOptions.formatOptions = FormatOptions.STANDARDBASELINE;
+
+	// matte (The color to use to fill anti-aliased edges adjacent to transparent areas of the image.
+	//			When transparency is turned off for an image, the matte color is applied to transparent areas.)
+	// Default: MatteType.WHITE
+	// Range: MatteType.BACKGROUND
+	//			MatteType.BLACK
+	//			MatteType.FOREGROUND
+	//			MatteType.NETSCAPE
+	//			MatteType.NONE
+	//			MatteType.SEMIGRAY
+	//			MatteType.WHITE
+	jpgSaveOptions.matte = MatteType.NONE;
+
+	// quality (The image quality setting to use; affects file size and compression)
+	// Default: 3
+	// Range: [0,12]
+	jpgSaveOptions.quality = quality;
+
+	// scans (The number of scans to make to incrementally display the image on the page.
+	//			Valid only for when formatOptions = FormatOptions.PROGRESSIVE.)
+	// Default: 3
+	// Range: [3,5]
+	//jpgSaveOptions.scans = 3
+
+	// typename (The class name of the referenced JPEGSaveOptions object.)
+	// Read only
+	//jpgSaveOptions.typename
+
+	app.activeDocument.saveAs(new File(saveFile), jpgSaveOptions, true,Extension.LOWERCASE);
+}
+
+
 // =====================
 // 导出jpg
 // =====================
 FILE=SCREENSHOT_PATH+"\\"+FILE_NAME+".jpg";
-// 文件指针
-var fileOut = new File(FILE);
-//fileOut.changePath(FILE)
-// 导出选项
-var exportOptions = new ExportOptionsSaveForWeb();
-//图片格式
-exportOptions.PNG8 = false;
-exportOptions.format=SaveDocumentType.JPEG;
-//压缩质量
-exportOptions.quality = 80;
-//保存图片
-app.activeDocument.exportDocument(fileOut, ExportType.SAVEFORWEB, exportOptions);
+SaveJPG(FILE,3)
