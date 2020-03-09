c4dd2783ee574c3fa7753033c8bc23c0759dc0bd
yemiancheng <ymc.github@gmail.com>
2020-03-09 21:03:35 +0800

    build: add config

---
+++ src/config-print.jsx
@@ -0,0 +1,31 @@
+// tell him run in ps
+#target photoshop
+
+// tell him include some .jsx file
+#include config.jsx
+#include utils.jsx
+
+// tell him do sth. here
+//alert(config.size.w)
+var val
+/*
+val=str2num(config.size.w)
+alert(val)
+*/
+/*
+val=caculateUnitValue(config.size.w)
+alert(val)
+*/
+/*
+txtOpts.pos.x=caculateUnitValue(txtOpts.pos.x)
+txtOpts.pos.y=caculateUnitValue(txtOpts.pos.y)
+txtOpts.area.w=caculateUnitValue(txtOpts.area.w)
+txtOpts.area.h=caculateUnitValue(txtOpts.area.h)
+txtOpts.size=caculateUnitValue(txtOpts.size)
+alert(txtOpts.size)
+*/
+
+/*
+val=htmlUnicodeChar2JsUnicodeChar("&#xf244;")
+alert(val);
+*/
---
+++ src/config.jsx
@@ -0,0 +1,43 @@
+#include utils.jsx
+//doc
+var config = {
+  size:{w:"500px",h:"500px"},
+  //resolution:300 //72
+  resolution:72
+}
+//bg
+var shape={
+  pos:{x:0,y:0},
+  //size:{w:500,h:500}
+  size:{w:100,h:100}
+}
+var innerShadowOpt={
+  //阻塞
+  choke:16,//%
+  //偏移
+  distance:0,//px
+  //透明度
+  opacity:100,//%
+  // 大小
+  size:shape.size.w*(50/1000)//px
+}
+//txt
+/*
+var txtOpts={
+  pos:{x:"0 px",y:"0 px"},
+  area:{w:"100 px",h:"100 px"},
+  size:"80 px",
+  font:"FontAwesome",
+  content:"\uf006",
+}
+*/
+var txtOpts={
+  pos:{x:"0 px",y:"0 px"},
+  //fix:use caculateUnitValue to fix txt area size when resolution>72
+  area:{w:caculateUnitValue("100 px"),h:caculateUnitValue("100 px")},
+  //fix:use caculateUnitValue to fix txt font size when resolution>72
+  size:caculateUnitValue("80 px"),
+  font:"FontAwesome",
+  content:"\uf006",//
+}
+//"&#xf26e;".replace("&#x","\u")
